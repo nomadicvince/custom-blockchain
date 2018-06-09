381c57783364bf516c502be56a0dfb8ed066386a
Vincent Moore <cosmiqwarrior@gmail.com>
2018-06-09 15:56:06 -0400

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-const Blockchain = require('./blockchain.js');
-const coin = new Blockchain();
-coin.createNewBlock(23456, '0IN546G', 'HTGT3499IJ');
-coin.createNewBlock(699669, 'WQS3269', 'FREF4564FG');
-coin.createNewBlock(4567, '0I9OPIG', 'HXBQ499IJ');
-
-console.log(coin);
